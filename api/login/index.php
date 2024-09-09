7f09332ce85c362fbf3f391e78424ae985c7a75a
Juan Manuel Alcivar <jalcivar@grupo-ditec.com.mx>
2024-09-08 18:02:59 -0600

    Actualizacion de metodos y postman colletcion , correccion de hader autorization header

--- api/login/index.php
+++ api/login/index.php
@@ -23,6 +23,13 @@ $app->addRoutingMiddleware();
  * for middleware added after it.
  */
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$app->add(function ($request, $handler) {
+      $response = $handler->handle($request);
+      return $response
+              ->withHeader('Access-Control-Allow-Origin', '*')
+              ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+              ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  });
 
 // Init define app routes
 // este metodo inicial tambien deberiamos escribirlo segun el dominio $response->getBody()->write("API LOGIN");
@@ -39,17 +46,6 @@ $app->get('/codigos', function (Request $request, Response $response, $args) {
     return $response;
 });
 
-$app->get('/enc/{secret}', function (Request $request, Response $response, $args) {
-  
-    //con este codigo al dar de alta los usuarios 
-    $secret =  password_hash( $args['secret'],PASSWORD_BCRYPT, ['cost' => 12,]);  
-    $response_data = [
-        'secret'  => $args['secret'],        
-        'encripted'  => $secret,        
-    ];
-    $response->getBody()->write( json_encode($response_data));
-    return $response->withHeader('Content-Type','application/json');
-});
 
 $app->post('/login', function (Request $request, Response $response, $args) use ($pdo) {
 
@@ -144,9 +140,15 @@ if($user){
 
 //metodos CRUD
 
-$app->post('/adduser', function (Request $request, Response $response, $args) use ($pdo) {
+$app->put('/adduser', function (Request $request, Response $response, $args) use ($pdo) {
     $headers = getallheaders();
-    $headerAut = $headers['Authorization'];
+    if(!array_key_exists('Authorization', $headers)){  
+        $response->getBody()->write( json_encode(['Codigo' => 666, 'msg' => "REQUIERE TOKEN DE AUTORIZACION"  ])); 
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }else{
+        $headerAut = $headers['Authorization'];
+    }
     $body = $request->getBody();
     $data = json_decode($body, true);
     try {
@@ -158,27 +160,23 @@ $app->post('/adduser', function (Request $request, Response $response, $args) us
 
             }
 //aqui inicia la generacion automatica de codigo
-    $user=$data['user'];
-	$password=$data['password'];
-	$name=$data['name'];
-	$lastname=$data['lastname'];
-	$secondlastname=$data['secondlastname'];
-	$email=$data['email'];
-	$status=$data['status'];
 
-    //FALTA VALIDAR
+    $user = array_key_exists('user', $data) ? $data['user'] : '';
+    $password = array_key_exists('password', $data) ? $data['password'] : '';
+    $name = array_key_exists('name', $data) ? $data['name'] : '';
+    $lastname = array_key_exists('lastname', $data) ? $data['lastname'] : '';
+    $secondlastname = array_key_exists('secondlastname', $data) ? $data['secondlastname'] : '';
+    $email = array_key_exists('email', $data) ? $data['email'] : '';
+    $status = array_key_exists('status', $data) ? $data['status'] : '';
+
+    if($user==""||$password=""||$name==""||$lastname==""||$secondlastname==""||$email==""||$status==""){
+        $responseDto = ['Codigo' => 3, 'msg' => 'TODOS LOS DATOS SON OBLIGATORIOS', 'body' => $body ];
+        $response->getBody()->write( json_encode($responseDto)  );
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }
+
     $passwordencriptada = password_hash($password,PASSWORD_BCRYPT, ['cost' => 12,]);
-/*
-   $consulta = $pdo->prepare("INSERT INTO `usersystem`(`ID`,`user`,`password`,`name`,`lastname`,`secondlastname`,`email`,`status`,`modifydate`,`modifyuser`)
-                                VALUES (NULL,':user',':password',':name',':lastname',':secondlastname',':email',':status',NOW(),':modifyuser')");
-  
-        
-        
-        
-        
-       
-        
-        ;*/
 
         $consulta = $pdo->prepare("INSERT INTO `usersystem`(`user`,`password`,`name`,`lastname`,`secondlastname`,`email`,`status`,`modifydate`,usrupd) 
         VALUES (:user,:password,:name,:lastname,:secondlastname,:email,:status,NOW(),:usrupd)");
@@ -192,9 +190,6 @@ $app->post('/adduser', function (Request $request, Response $response, $args) us
         $consulta->bindParam(':usrupd',$decoded_array['user']);
         $consulta->execute();
 
-
-
-        
         $user_id = $pdo->lastInsertId();
         if($user_id != 0){
             $body = [   'id' => $user_id 
@@ -210,63 +205,235 @@ $app->post('/adduser', function (Request $request, Response $response, $args) us
         }
 
 
-//aqui inicia la generacion automatica de codigo
+//aqui termina la generacion automatica de codigo
             $response->getBody()->write( json_encode($responseDto)  );
-
     } catch (Exception $e) {
         $response->getBody()->write( json_encode(['Codigo' => 2, 'msg' => $e->getMessage() , 'body' => '']) );
     } finally {
         return $response;      
     }
-
-
   return $response->withHeader('Content-Type','application/json');
 });
 
 
 
 
+$app->patch('/updateuser/{id}', function (Request $request, Response $response, $args) use ($pdo) {
+    $headers = getallheaders();
+    if(!array_key_exists('Authorization', $headers)){  
+        $response->getBody()->write( json_encode(['Codigo' => 666, 'msg' => "REQUIERE TOKEN DE AUTORIZACION"  ])); 
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }else{
+        $headerAut = $headers['Authorization'];
+    }
+    $body = $request->getBody();
+    $data = json_decode($body, true);
+    //aqui va el PK de la tabla
+    $id = array_key_exists('id', $args) ? $args['id'] : '';
+    //aqui va el PK de la tabla
+    try {
+        $secret_Key  = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+        list($type, $jwt) = explode(' ', $headerAut, 2);
+            if (strcasecmp($type, 'Bearer') == 0) {
+            $decoded = JWT::decode($jwt, new Key($secret_Key, 'HS512'));
+            $decoded_array = (array) $decoded;
 
-$app->get('/test', function (Request $request, Response $response, $args) {
-
-    $key = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
-    $date   = new DateTimeImmutable();
-    $expire_at     = $date->modify('+30 minutes')->getTimestamp();  
-    $domainName = "your.domain.name";
-    $usr ="juanma";
-    $usrname="juanma";
-    $lastname="juanma";
-    $secondlastname="juanma";
-    $email="juanma";
-    $sysname="juanma";
-    $status="juanma";
-
-
+            }
+//aqui inicia la generacion automatica de codigo
     
-    $payload = [
-        'iat'  => $date->getTimestamp(),         // Issued at: time when the token was generated
-        'iss'  => $domainName,                       // Issuer
-        'nbf'  => $date->getTimestamp(),         // Not before
-        'exp'  => $expire_at,
-        'user' => $usr,
-        'usrname' => $usrname,
-        'lastname' => $lastname,
-        'secondlastname' => $secondlastname,
-        'email' => $email,
-        'sysname' => $sysname,
-        'status' => $status,
- ];
-    
- $jwt = JWT::encode($payload, $key, 'HS256');
-    
-     
- $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
- //print_r($decoded);
- 
- 
-     $response->getBody()->write( $jwt);
-     return $response;
+    $user = array_key_exists('user', $data) ? $data['user'] : '';
+    $password = array_key_exists('password', $data) ? $data['password'] : '';
+    $name = array_key_exists('name', $data) ? $data['name'] : '';
+    $lastname = array_key_exists('lastname', $data) ? $data['lastname'] : '';
+    $secondlastname = array_key_exists('secondlastname', $data) ? $data['secondlastname'] : '';
+    $email = array_key_exists('email', $data) ? $data['email'] : '';
+    $status = array_key_exists('status', $data) ? $data['status'] : '';
+
+    if($id==""||$user==""||$password=""||$name==""||$lastname==""||$secondlastname==""||$email==""||$status==""){
+        $responseDto = ['Codigo' => 3, 'msg' => 'TODOS LOS DATOS SON OBLIGATORIOS', 'body' => $body ];
+        $response->getBody()->write( json_encode($responseDto)  );
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }
+
+    $passwordencriptada = password_hash($password,PASSWORD_BCRYPT, ['cost' => 12,]);
+
+        $consulta = $pdo->prepare("UPDATE `usersystem` 
+                                                      SET `user` = :user
+                                                    ,  `password` = :password 
+                                                    ,  `name` = :name 
+                                                    ,  `lastname` = :lastname
+                                                    ,  `secondlastname` = :secondlastname
+                                                    ,  `email` = :email
+                                                    ,  `status` = :status 
+                                                    ,  `modifydate` = NOW()
+                                                    ,  `usrupd` = :usrupd
+                                                      WHERE `id` = :id ");
+        
+        $consulta->bindParam(':user',$data['user']);
+        $consulta->bindParam(':password',$passwordencriptada);
+        $consulta->bindParam(':name',$data['name']);
+        $consulta->bindParam(':lastname',$data['lastname']);
+        $consulta->bindParam(':secondlastname',$data['secondlastname']);
+        $consulta->bindParam(':email',$data['email']);
+        $consulta->bindParam(':status',$data['status']);
+        $consulta->bindParam(':usrupd',$decoded_array['user']);
+        $consulta->bindParam(':id',$id);
+        
+        if($consulta->execute()){
+            $body = [   
+              'id' => $id 
+            , 'name' => $data['name']
+            , 'lastname' => $data['lastname'] 
+            , 'secondlastname' => $data['secondlastname'] 
+            , 'email' => $data['email'] 
+            , 'status' => $data['status'] 
+        ];
+            $responseDto = ['Codigo' => 1, 'msg' => 'USUARIO ACTUALIZADO CORRECTAMENTE', 'body' => $body ];
+        }else{
+            $responseDto = ['Codigo' => 2, 'msg' => 'OCURRIO UN ERROR', 'body' => ''];
+        }
+
+
+//aqui termina la generacion automatica de codigo
+            $response->getBody()->write( json_encode($responseDto)  );
+    } catch (Exception $e) {
+        $response->getBody()->write( json_encode(['Codigo' => 2, 'msg' => $e->getMessage() , 'body' => '']) );
+    } finally {
+        return $response;      
+    }
+  return $response->withHeader('Content-Type','application/json');
 });
+
+
+
+
+$app->delete('/deleteuser/{id}', function (Request $request, Response $response, $args) use ($pdo) {
+    $headers = getallheaders();
+    if(!array_key_exists('Authorization', $headers)){  
+        $response->getBody()->write( json_encode(['Codigo' => 666, 'msg' => "REQUIERE TOKEN DE AUTORIZACION"  ])); 
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }else{
+        $headerAut = $headers['Authorization'];
+    }
+    $body = $request->getBody();
+    $data = json_decode($body, true);
+    //aqui va el PK de la tabla
+    $id = array_key_exists('id', $args) ? $args['id'] : '';
+    //aqui va el PK de la tabla
+    try {
+        $secret_Key  = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+        list($type, $jwt) = explode(' ', $headerAut, 2);
+            if (strcasecmp($type, 'Bearer') == 0) {
+            $decoded = JWT::decode($jwt, new Key($secret_Key, 'HS512'));
+            $decoded_array = (array) $decoded;
+
+            }
+//aqui inicia la generacion automatica de codigo
+    
+    if($id==""){
+        $responseDto = ['Codigo' => 3, 'msg' => 'TODOS LOS DATOS SON OBLIGATORIOS', 'body' => $body ];
+        $response->getBody()->write( json_encode($responseDto)  );
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }
+        $consulta = $pdo->prepare("DELETE FROM `usersystem` WHERE `id` = :id ");
+        $consulta->bindParam(':id',$id);
+        
+        if($consulta->execute()){
+            $responseDto = ['Codigo' => 1, 'msg' => 'USUARIO ELIMINADO CORRECTAMENTE', 'body' => '' ];
+        }else{
+            $responseDto = ['Codigo' => 2, 'msg' => 'OCURRIO UN ERROR', 'body' => ''];
+        }
+//aqui termina la generacion automatica de codigo
+            $response->getBody()->write( json_encode($responseDto)  );
+    } catch (Exception $e) {
+        $response->getBody()->write( json_encode(['Codigo' => 2, 'msg' => $e->getMessage() , 'body' => '']) );
+    } finally {
+        return $response;      
+    }
+  return $response->withHeader('Content-Type','application/json');
+});
+
+
+$app->get('/getall', function (Request $request, Response $response, $args) use ($pdo) {
+
+    $headers = getallheaders();
+    if(!array_key_exists('Authorization', $headers)){  
+        $response->getBody()->write( json_encode(['Codigo' => 666, 'msg' => "REQUIERE TOKEN DE AUTORIZACION"  ])); 
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }else{
+        $headerAut = $headers['Authorization'];
+    }
+
+    try {
+        $secret_Key  = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+        list($type, $jwt) = explode(' ', $headerAut, 2);
+            if (strcasecmp($type, 'Bearer') == 0) {
+            $decoded = JWT::decode($jwt, new Key($secret_Key, 'HS512'));
+            $decoded_array = (array) $decoded;
+
+            }
+//aqui inicia la generacion automatica de codigo
+    
+$consulta = $pdo->query("SELECT `user`,`password`,`name`,`lastname`,`secondlastname`,`email`,`status`,`modifydate`,`usrupd`  FROM usersystem ");
+$dbdata = $consulta->fetchAll(PDO::FETCH_ASSOC);
+$response->getBody()->write(json_encode($dbdata));
+
+//aqui termina la generacion automatica de codigo
+} catch (Exception $e) {
+    $response->getBody()->write( json_encode(['Codigo' => 2, 'msg' => $e->getMessage() , 'body' => '']) );
+} 
+
+return $response->withHeader('Content-Type','application/json');
+
+});
+
+
+ 
+$app->get('/geuserbyid/{id}', function (Request $request, Response $response, $args) use ($pdo) {
+
+    $headers = getallheaders();
+    if(!array_key_exists('Authorization', $headers)){  
+        $response->getBody()->write( json_encode(['Codigo' => 666, 'msg' => "REQUIERE TOKEN DE AUTORIZACION"  ])); 
+        return $response->withHeader('Content-Type','application/json');
+        exit;
+    }else{
+        $headerAut = $headers['Authorization'];
+    }
+
+    //aqui va el PK de la tabla
+    $id = array_key_exists('id', $args) ? $args['id'] : '';
+    //aqui va el PK de la tabla
+
+    try {
+        $secret_Key  = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+        list($type, $jwt) = explode(' ', $headerAut, 2);
+            if (strcasecmp($type, 'Bearer') == 0) {
+            $decoded = JWT::decode($jwt, new Key($secret_Key, 'HS512'));
+            $decoded_array = (array) $decoded;
+
+            }
+//aqui inicia la generacion automatica de codigo
+    
+$consulta = $pdo->query("SELECT `user`,`password`,`name`,`lastname`,`secondlastname`,`email`,`status`,`modifydate`,`usrupd`  FROM usersystem where `id` = $id");
+
+$dbdata = $consulta->fetchAll(PDO::FETCH_ASSOC);
+$response->getBody()->write(json_encode($dbdata));
+
+//aqui termina la generacion automatica de codigo
+} catch (Exception $e) {
+    $response->getBody()->write( json_encode(['Codigo' => 2, 'msg' => $e->getMessage() , 'body' => '']) );
+} 
+
+return $response->withHeader('Content-Type','application/json');
+
+});
+
+
 
 // end define app routes
 // Run app
